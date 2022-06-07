27579e3fc8beae761b7a56996f9c07c987ff265b
Burak <burak@90pixel.com>
2022-06-07 23:20:49 +0300

    Update main app

--- index.ts
+++ index.ts
@@ -1,32 +1,24 @@
 import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
-
+import { cityLinks } from 'utils';
 dotenv.config();
 
 const mainUrl = process.env.WEB_PAGE;
 
-const test = async () => {
+const app = async () => {
   console.log('started to collecting');
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const pageURL = String(`${mainUrl}/mugla-otelleri-telefon.html`);
+  const pageURL = `${mainUrl}/mugla-otelleri-telefon.html`;
   await page.goto(pageURL);
 
-  const cardLinks = await page.$$eval('.pr-card-link', (nodes) =>
-    nodes.map((val) => val.getAttribute('href'))
-  );
-  console.log(cardLinks);
+  const cityCardLinks = await cityLinks({ page });
 
-  page.goto(`${mainUrl}/mugla-akyaka-otelleri-iletisim.html`);
+  console.log(cityCardLinks);
 
-  // for (let i = 0; i < cardLinks.length; i++) {
-  //   // const properties = await cardLinks[i]
-  //   console.log(cardLinks[i]);
-  // }
-
-  // await browser.close();
+  await browser.close();
   console.log('finished collecting');
 };
 
-test();
+app();
