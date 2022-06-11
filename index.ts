a8d70f79793aab2a06c94ef6a5d4b21f6798bad3
Burak <burak@90pixel.com>
2022-06-11 21:41:09 +0300

    Update main function.

--- index.ts
+++ index.ts
@@ -1,52 +1,68 @@
 import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
 
-import { getCityLinks, getOtelLinks, getOtelDetail, createExcel } from 'utils';
+import {
+  getCityLinks,
+  getOtelLinks,
+  getOtelDetail,
+  createExcel,
+  logger
+} from 'utils';
+
 dotenv.config();
 
 const mainUrl = process.env.WEB_PAGE;
 
 const app = async () => {
-  console.log('started collecting');
+  // console.log('started collecting');
+  logger({ logType: 'warn', content: 'started collecting' });
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   const pageURL = `${mainUrl}/mugla-otelleri-telefon.html`;
   await page.goto(pageURL);
 
-  console.log('started collecting city links');
-
+  logger({ logType: 'warn', content: 'started collecting city links!' });
   const cityLinks = await getCityLinks({ page });
-  console.log('successfully collected city links');
+  logger({ logType: 'success', content: 'successfully collected city links' });
 
-  console.log('started collecting otel details');
+  logger({ logType: 'warn', content: 'started collecting otel details' });
   for (const cityUrl of cityLinks) {
     const cityName = cityUrl.split('-')[1];
 
-    console.log(`started collecting ${cityName} `);
+    logger({ logType: 'warn', content: `collecting ${cityName}'s links` });
     const otelLinks = await getOtelLinks({
       page,
       cityUrl
     });
-    console.log(`successfully collected ${cityName}s otel links `);
+    logger({
+      logType: 'success',
+      content: `successfully collected ${cityName}s otel links`
+    });
 
     for (const otelUrl of otelLinks) {
       const otelName = otelUrl.split('-telefon')[0];
+      logger({
+        logType: 'warn',
+        content: `collecting details for ${otelName}`
+      });
 
       const otelDetail = await getOtelDetail({
         page,
         otelUrl,
         otelLocation: cityName
       });
+      console.log(otelDetail);
 
-      // console.log({ location: cityName, otelName, otelDetail });
+      logger({
+        logType: 'success',
+        content: `successflully collected data for ${otelName}`
+      });
     }
   }
 
-  console.log('successflully collected otel detail');
-
   await browser.close();
-  console.log('finished collecting');
+  logger({ logType: 'success', content: 'finished collecting data' });
 
   // console.log('creating excel file from collected data');
   // createExcel();
