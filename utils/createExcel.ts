de9bcb9ae295387b4e8595be2a826371e5ef5e34
Burak <burak@90pixel.com>
2022-06-11 23:09:57 +0300

    Update createExcel method.

--- utils/createExcel.ts
+++ utils/createExcel.ts
@@ -1,21 +1,12 @@
 import { utils, writeFile } from 'xlsx';
-const dummyData = [
-  {
-    firstName: 'Burak',
-    lastName: 'Çardak'
-  },
-  {
-    firstName: 'Murat',
-    lastName: 'Çardak'
-  }
-];
+import { DetailProps } from 'types';
 
 /**
  * Json halinde gelen veriyi, xlsx formatına çevirir ve
  * İletişim bilgilerinin bulunduğu bir excel dosyası oluşturur.
  */
-const createCSV = () => {
-  const worksheet = utils.json_to_sheet(dummyData);
+const createCSV = (data: DetailProps[]) => {
+  const worksheet = utils.json_to_sheet(data);
 
   const workbook = utils.book_new();
   utils.book_append_sheet(workbook, worksheet, 'Contact Informations');
