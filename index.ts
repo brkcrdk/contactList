75321b0662d3db0c8b398d2c40b2b63bb8da5310
Burak <burak@90pixel.com>
2022-08-05 21:05:22 +0300

    Remove old structure.

--- index.ts
+++ index.ts
@@ -16,65 +16,16 @@ dotenv.config();
 const mainUrl = process.env.WEB_PAGE;
 
 const app = async () => {
-  const data: DetailProps[] = [];
-  let counter = 0;
-  // console.log('started collecting');
   logger({ logType: 'warn', content: 'started collecting' });
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
 
-  const pageURL = `${mainUrl}/mugla-otelleri-telefon.html`;
-  await page.goto(pageURL);
+  logger({ logType: 'warn', content: 'started collecting city names' });
+  const cities = await getCityLinks(browser);
+  logger({ logType: 'success', content: 'successfully collected city names' });
 
-  logger({ logType: 'warn', content: 'started collecting city links!' });
-  const cityLinks = await getCityLinks({ page });
-  logger({ logType: 'success', content: 'successfully collected city links' });
-
-  logger({ logType: 'warn', content: 'started collecting otel details' });
-  for (const cityUrl of cityLinks) {
-    const cityName = cityUrl.split('-')[1];
-
-    logger({ logType: 'warn', content: `collecting ${cityName}'s links` });
-    const otelLinks = await getOtelLinks({
-      page,
-      cityUrl
-    });
-    logger({
-      logType: 'success',
-      content: `successfully collected ${cityName}s otel links`
-    });
-
-    for (const otelUrl of otelLinks) {
-      const otelName = otelUrl.split('-telefon')[0];
-
-      const otelDetail = await getOtelDetail({
-        page,
-        otelUrl,
-        otelLocation: cityName
-      });
-      const { email, phoneNumber } = otelDetail;
-      if (email || phoneNumber) {
-        data.push(otelDetail);
-        counter++;
-        logger({
-          logType: 'info',
-          content: `successfully collected => ${otelName} ~ ${cityName} #${counter}`
-        });
-      }
-    }
-  }
+  console.log(cities);
 
   await browser.close();
-  console.log(data);
-  logger({ logType: 'success', content: 'finished collecting data' });
-
-  // console.log('creating excel file from collected data');
-  logger({
-    logType: 'warn',
-    content: 'creating excel file from collected data'
-  });
-  createExcel(data);
-  logger({ logType: 'success', content: 'excel file is created' });
 };
 
 app();
